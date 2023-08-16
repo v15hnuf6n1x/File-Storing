a97c9a6d21c20547c4dbda06978f5c31d5cddecd
VISHNU <Vishnuvishva2434@gmail.com>
2023-08-17 00:08:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
+API_ID = int(os.environ.get("21165589", 0))
+API_HASH = os.environ.get("8cc762f4873e84a7cf0cbfd66a07244b")
+BOT_TOKEN = os.environ.get("B6517235191:AAF0A550ymSnlbPyNJvf7otys0v6mwuluWk")
+DB_CHANNEL_ID = os.environ.get("-1001869648454")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("2048030675"))
 PROTECT_CONTENT = True
 UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
