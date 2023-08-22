1f5e3f98f145d958d0b29ebd242009fb94a7dbfe
VISHNU <Vishnuvishva2434@gmail.com>
2023-08-22 23:14:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ BOT_TOKEN = os.environ.get("p6517235191:AAF0A550ymSnlbPyNJvf7otys0v6mwuluWk")
 DB_CHANNEL_ID = os.environ.get("-1001869648454")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("2048030675")
-PROTECT_CONTENT = False 
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+ UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
